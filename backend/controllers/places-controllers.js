8847404287e5fde919aa941094271d9f695fb823
강동욱 <ehddnr8563@gmail.com>
2024-05-24 01:33:16 +0900

    feat: 장소 업데이트 라우트 db 연동

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -20,32 +20,55 @@ let DUMMY_PLACES = [
   },
 ];
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pId;
 
-  const place = DUMMY_PLACES.find((place) => place.id === placeId);
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      '오류가 발생했습니다. 장소를 찾을 수 없습니다.',
+      500
+    );
+    return next(error);
+  }
 
   if (!place) {
     // 동기식 미들웨어에는 throw 사용 가능.
     // (하지만 대부분 데이터베이스 연결과 같은 비동기 미들웨어 사용하기 때문에 next 사용 해야함)
-    throw new HttpError('해당 ID에 대한 장소를 찾지 못했습니다.', 404);
+    const error = new HttpError('해당 ID에 대한 장소를 찾지 못했습니다.', 404);
+    return next(error);
   }
 
-  res.json({ place });
+  // mongoose 고유 객체를 일반 자바스크립트 객체로 변환해서 응답
+  res.json({ place: place.toObject({ getters: true }) });
 };
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uId;
 
-  const places = DUMMY_PLACES.filter((place) => place.creator === userId);
-
-  if (!places || places.length === 0) {
-    return next(
-      new HttpError('해당 사용자 ID에 대한 장소들을 찾지 못했습니다.', 404)
-    ); // return 반드시 추가. next는 중단 하지 않음.
+  let places;
+  try {
+    places = (await Place.find({ creator: userId })).map((place) =>
+      place.toObject({ getters: true })
+    );
+  } catch (err) {
+    const error = new HttpError(
+      '오류가 발생했습니다. 장소들을 찾을 수 없습니다.',
+      500
+    );
+    return next(error);
   }
 
-  res.json({ places });
+  if (!places || places.length === 0) {
+    // ! return 반드시 추가. next는 중단 하지 않음.
+    return next(
+      new HttpError('해당 사용자 ID에 대한 장소들을 찾지 못했습니다.', 404)
+    );
+  }
+
+  res.json({ places: places });
 };
 
 const createPlace = async (req, res, next) => {
@@ -94,7 +117,7 @@ const createPlace = async (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError(
@@ -106,16 +129,30 @@ const updatePlace = (req, res, next) => {
   const placeId = req.params.pId;
   const { title, description } = req.body;
 
-  const updatedPlace = {
-    ...DUMMY_PLACES.find((place) => place.id === placeId),
-  };
-  const placeIndex = DUMMY_PLACES.findIndex((place) => place.id === placeId);
-  updatedPlace.title = title;
-  updatedPlace.description = description;
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      '오류가 발생했습니다. 장소를 수정할 수 없습니다.',
+      500
+    );
+    return next(error);
+  }
+  place.title = title;
+  place.description = description;
 
-  DUMMY_PLACES[placeIndex] = updatedPlace;
+  try {
+    await place.save();
+  } catch (err) {
+    const error = new HttpError(
+      '오류가 발생했습니다. 장소를 수정할 수 없습니다.',
+      500
+    );
+    return next(error);
+  }
 
-  res.status(200).json({ place: updatedPlace });
+  res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
 const deletePlace = (req, res, next) => {
