1b7a31f473b2be592b879c6a92aa6b07d15beee6
강동욱 <ehddnr8563@gmail.com>
2024-05-28 15:16:52 +0900

    feat: 회원가입, 로그인, 사용자 리스트 api 구현

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,59 +1,84 @@
-const uuid = require('uuid').v4;
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const User = require('../models/user');
 
-const DUMMY_USERS = [
-  {
-    id: 'u1',
-    name: '강동욱',
-    email: 'test@test.com',
-    password: '12345678',
-  },
-];
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, '-password');
+  } catch (err) {
+    const error = new HttpError(
+      '사용자 리스트를 가져오는데 실패하였습니다. 나중에 다시 시도해주세요.',
+      500
+    );
+    return next(error);
+  }
 
-const getUsers = (req, res, next) => {
-  res.status(200).json({
-    message: 'success fetch user list',
-    users: DUMMY_USERS,
-  });
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
-const signUp = (req, res, next) => {
+const signUp = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError(
-      '유효하지 않은 입력 데이터를 전달했습니다. 데이터를 확인하세요.',
-      422
+    return next(
+      new HttpError(
+        '유효하지 않은 입력 데이터를 전달했습니다. 데이터를 확인하세요.',
+        422
+      )
     );
   }
 
   const { name, email, password } = req.body;
 
-  const hasUser = DUMMY_USERS.find((user) => user.email === email);
-  if (hasUser) {
-    throw new HttpError(
-      '이메일이 이미 존재하므로 사용자를 생성할 수 없습니다.',
-      422
+  // uniqueValidator 패키지에서 불러온 유효성 검사를 사용하면 기술적인 메시지가 전달될 가능성 존재
+  // 그래서 수동 유효성 검사 로직 추가.
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      '회원가입에 실패했으니 나중에 다시 시도하세요.',
+      500
     );
+    return next(error);
   }
 
-  const createdUser = {
-    id: uuid(),
+  if (existingUser) {
+    const error = new HttpError(
+      '해당 이메일을 사용하는 사용자가 이미 존재합니다. 로그인 하세요.',
+      422
+    );
+    return next(error);
+  }
+
+  const createdUser = new User({
     name,
     email,
+    image:
+      'https://plus.unsplash.com/premium_photo-1669253767213-404f6888e895?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHx8',
     password,
-  };
+    places: [],
+  });
 
-  DUMMY_USERS.push(createdUser);
+  try {
+    // 모델의 인스턴스로 데이터베이스 문서 생성
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError(
+      '회원가입에 실패했습니다. 다시 시도하세요.',
+      500
+    );
+    return next(error);
+  }
 
   res.status(201).json({
-    message: 'success create user and login',
-    user: createdUser,
+    message: '회원가입에 성공하였습니다.',
+    user: createdUser.toObject({ getters: true }),
   });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError(
@@ -64,17 +89,28 @@ const login = (req, res, next) => {
 
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError(
-      '사용자를 식별할 수 없으니 자격 증명을 확인하세요.',
-      401
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      '로그인에 실패했으니 나중에 다시 시도하세요.',
+      500
     );
+    return next(error);
   }
 
-  res.status(200).json({
-    message: 'success login',
-    user: loginUser,
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      '유효하지 않은 자격 증명으로 인해 로그인할 수 없습니다.',
+      401
+    );
+    return next(error);
+  }
+
+  res.json({
+    message: '로그인에 성공하였습니다.',
+    user: existingUser,
   });
 };
 
