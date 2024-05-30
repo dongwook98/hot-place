4be76e3acf10edb70114c2ab35c95b69e4befa6c
강동욱 <ehddnr8563@gmail.com>
2024-05-30 23:52:09 +0900

    feat: 장소 추가 api 이미지 파일 미들웨어 등록 및 수정

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -45,7 +45,7 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!places) {
     // ! return 반드시 추가. next는 중단 하지 않음.
     return next(
       new HttpError('해당 사용자 ID에 대한 장소들을 찾지 못했습니다.', 404)
@@ -83,8 +83,7 @@ const createPlace = async (req, res, next) => {
     description,
     address,
     location: coordinates,
-    image:
-      'https://plus.unsplash.com/premium_photo-1669253767213-404f6888e895?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHx8',
+    image: req.file.path,
     creator,
   });
 
