06a7055bb71c0e4baad69318d4be965cfcba70ef
강동욱 <ehddnr8563@gmail.com>
2024-05-30 16:08:43 +0900

    feat: 스팟 수정 api 로직 작성

--- frontend/src/places/pages/UpdatePlace.jsx
+++ frontend/src/places/pages/UpdatePlace.jsx
@@ -1,48 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 
 import './PlaceForm.css';
 import Input from '../../shared/components/FormElement/Input';
 import Button from '../../shared/components/FormElement/Button';
 import Card from '../../shared/components/UIElement/Card';
+import LoadingSpinner from '../../shared/components/UIElement/LoadingSpinner';
+import ErrorModal from '../../shared/components/UIElement/ErrorModal';
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
-
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: '엠파이어 스테이트 빌딩',
-    description: 'One of the Most famous sky scrapers in the world!',
-    imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Empire_States_Building.jpg/275px-Empire_States_Building.jpg',
-    address: '20 W 34th St., New York, NY 10001 미국',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9856644,
-    },
-    creator: 'u1',
-  },
-  {
-    id: 'p2',
-    title: '엠파이어 스테이트 빌딩',
-    description: 'One of the Most famous sky scrapers in the world!',
-    imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Empire_States_Building.jpg/275px-Empire_States_Building.jpg',
-    address: '20 W 34th St., New York, NY 10001 미국',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9856644,
-    },
-    creator: 'u2',
-  },
-];
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from '../../shared/context/auth-context';
 
 const UpdatePlace = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedPlace, setLoadedPlace] = useState();
+  const auth = useContext(AuthContext);
   const placeId = useParams().placeId;
+  const history = useHistory();
 
   const [formState, inputHandler, setFormData] = useForm(
     // 더미 데이터
@@ -59,34 +37,60 @@ const UpdatePlace = () => {
     false
   );
 
-  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId); // 백엔드 요청
-
   useEffect(() => {
-    // API 응답 후 작업
-    if (identifiedPlace) {
-      setFormData(
-        {
-          title: {
-            value: identifiedPlace.title,
-            isValid: true,
+    const fetchPlace = async () => {
+      try {
+        const responseData = await sendRequest(
+          `${process.env.REACT_APP_API_URL}/api/places/${placeId}`
+        );
+        setLoadedPlace(responseData.place);
+        setFormData(
+          {
+            title: {
+              value: responseData.place.title,
+              isValid: true,
+            },
+            description: {
+              value: responseData.place.description,
+              isValid: true,
+            },
           },
-          description: {
-            value: identifiedPlace.description,
-            isValid: true,
-          },
-        },
-        true
-      );
-    }
-    setIsLoading(false);
-  }, [setFormData, identifiedPlace]);
+          true
+        );
+      } catch (err) {}
+    };
+    fetchPlace();
+  }, [placeId, sendRequest, setFormData]);
 
-  const placeUpdateSubmitHandler = (event) => {
+  const placeUpdateSubmitHandler = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
+
+    try {
+      await sendRequest(
+        `${process.env.REACT_APP_API_URL}/api/places/${placeId}`,
+        'PATCH',
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+        }),
+        {
+          'Content-Type': 'application/json',
+        }
+      );
+
+      history.push(`/${auth.userId}/places`);
+    } catch (error) {}
   };
 
-  if (!identifiedPlace) {
+  if (isLoading) {
+    return (
+      <div className='center'>
+        <LoadingSpinner asOverlay />
+      </div>
+    );
+  }
+
+  if (!loadedPlace && !error) {
     return (
       <div className='center'>
         <Card>
@@ -96,42 +100,37 @@ const UpdatePlace = () => {
     );
   }
 
-  if (isLoading) {
-    return (
-      <div className='center'>
-        <h2>로딩중...</h2>
-      </div>
-    );
-  }
-
   // Input 컴포넌트의 initialValue는 데이터 채워준 후 전달 되어야함. (state가 아님)
   return (
-    <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
-      <Input
-        id='title'
-        element='input'
-        type='text'
-        label='제목'
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText='필수 입력 항목입니다.'
-        onInput={inputHandler}
-        initialValue={formState.inputs.title.value}
-        initialValid={formState.inputs.title.isValid}
-      />
-      <Input
-        id='description'
-        element='textarea'
-        label='설명'
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText='최소 5글자 이상이어야 합니다.'
-        onInput={inputHandler}
-        initialValue={formState.inputs.description.value}
-        initialValid={formState.inputs.description.isValid}
-      />
-      <Button type='submit' disabled={!formState.isFormValid}>
-        수정
-      </Button>
-    </form>
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
+        <Input
+          id='title'
+          element='input'
+          type='text'
+          label='제목'
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText='필수 입력 항목입니다.'
+          onInput={inputHandler}
+          initialValue={loadedPlace.title}
+          initialValid={true}
+        />
+        <Input
+          id='description'
+          element='textarea'
+          label='설명'
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText='최소 5글자 이상이어야 합니다.'
+          onInput={inputHandler}
+          initialValue={loadedPlace.description}
+          initialValid={true}
+        />
+        <Button type='submit' disabled={!formState.isFormValid}>
+          수정
+        </Button>
+      </form>
+    </React.Fragment>
   );
 };
 
