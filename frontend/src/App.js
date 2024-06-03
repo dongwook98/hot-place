83d5db6ee611750c367c704ab661cd603c2fbaee
강동욱 <ehddnr8563@gmail.com>
2024-06-03 23:57:04 +0900

    feat: 토큰 만료 기한 설정 및 자동 로그아웃 기능 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,32 +14,61 @@ import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import { AuthContext } from './shared/context/auth-context';
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState();
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState();
 
-  const login = useCallback((userId, token) => {
+  const login = useCallback((userId, token, expirationDate) => {
     setToken(token);
     setUserId(userId);
+    // 토큰 만료 기한
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
     window.localStorage.setItem(
       'userData',
       JSON.stringify({
         userId,
         token,
+        expiration: tokenExpirationDate.toISOString(),
       })
     );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
     window.localStorage.removeItem('userData');
   }, []);
 
   useEffect(() => {
+    if (token && tokenExpirationDate) {
+      // 토큰 기한에 따른 자동 로그아웃
+      const remainingTime = tokenExpirationDate.getTime() - new Date();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      // 수동 로그아웃
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  // 새로고침 시 다시 로그인 되게하는 로직
+  useEffect(() => {
     const storedData = JSON.parse(window.localStorage.getItem('userData'));
-    if (storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
     }
   }, [login]);
 
